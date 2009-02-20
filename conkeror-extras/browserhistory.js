3ad555b13bb0aa160fe446fd99633aab3e225a60
Luke Opperman <luke@loppear.com>
2009-02-20 10:46:33 -0600

    conkeror browserhistory logging continued

--- conkeror-extras/browserhistory.js
+++ conkeror-extras/browserhistory.js
@@ -24,12 +24,17 @@ command_advice("find-url-new-buffer", find_after, $type="after");
 
 function make_watch(txt) {
   return function watch_location_change(buffer) {
-    var url = buffer.current_URI && buffer.current_URI.spec || "()"
-    dumpln(txt+" to "+url);
+
+    if (buffer.current_URI) {
+      dumpln(txt+" to "+ buffer.current_URI.spec);
+    }
   };
 }
-
+/*
 add_hook("current_content_buffer_location_change_hook",
          make_watch("change"));
 add_hook("content_buffer_finished_loading_hook",
          make_watch("open"));
+*/
+add_hook("buffer_description_change_hook",
+         make_watch("desc"));
