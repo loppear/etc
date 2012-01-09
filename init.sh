bd855a40d95a64b4d219f742db92e314cec55b0b
Luke Opperman <luke@loppear.com>
2012-01-09 13:10:32 -0600

    init updates for fresh 11.10 install

--- init.sh
+++ init.sh
@@ -1,28 +1,29 @@
 #!/bin/sh
 #
 
-sudo apt-get install emacs-snapshot
+sudo apt-get install emacs
 sudo apt-get install rxvt-unicode
 sudo apt-get install xmonad
 sudo apt-get install ipython
 sudo apt-get install conkeror
 sudo apt-get install mercurial
 sudo apt-get install git
-sudo apt-get install ttf-dejavu
+sudo apt-get install ttf-dejavu ttf-inconsolata
 sudo apt-get install python-dulwich
 sudo apt-get install python-subversion
 sudo apt-get install curl
+sudo apt-get install python-pip
+sudo pip install mercurial_keyring
+
 
 mkdir -p ~/lib/
 hg clone http://bitbucket.org/durin42/dotfiles ~/lib/durin42-dotfiles
 hg clone http://bitbucket.org/durin42/nosemacs ~/lib/nosemacs
 hg clone http://bitbucket.org/durin42/hgsubversion ~/lib/hgsubversion
 hg clone http://bitbucket.org/durin42/hg-git ~/lib/hggit
-hg clone http://bitbucket.org/ebo/hggnome-keyring ~/lib/hggnome-keyring
 hg clone http://js2-mode.googlecode.com/svn/trunk/ ~/lib/js2-mode
 hg clone http://hg.intevation.org/mercurial/crew ~/lib/mercurial-crew
 hg clone http://bitbucket.org/durin42/histedit/ ~/lib/histedit
-hg clone git://github.com/mattharrison/pycoverage.el.git ~/lib/pycoverage.el
 hg clone git://github.com/defunkt/coffee-mode.git ~/lib/coffee-mode
 
 mkdir -p ~/.xmonad/
